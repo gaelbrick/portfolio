8664a270222584fc60baadeeb2820d888709f618
Gael Ramirez <gael1423@gmail.com>
2022-05-12 12:39:47 -0500

    contact update

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,26 @@ linkWork.forEach(L=> L.addEventListener('click', activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
+let swiperTestimonial = new Swiper(".testimonial_container", {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
